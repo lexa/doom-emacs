3db2539dab53d1680e655b336b87654108dbe761
Aleksei Fedotov <aleksei@fedotov.email>
2019-05-24 16:39:51 +0200

    emacs: Add smart open file.

--- modules/config/default/+emacs.el
+++ modules/config/default/+emacs.el
@@ -53,3 +53,19 @@
 (setq browse-url-browser-function
       '(("https?://.*\\.youtube.com/watch\\?v=" . (lambda (url &rest args) (async-shell-command (concat "mpv \'" url"\'") "*Youtube MPV*")))
         (".*" . browse-url-default-browser)))
+
+;; helps open file file:line
+(defadvice server-visit-files (before parse-numbers-in-lines (files proc &optional nowait) activate)
+  "Looks for filenames like file:line or file:line:position and reparses name in such manner that position in file"
+  (ad-set-arg 0
+              (mapcar (lambda (fn)
+                        (let ((name (car fn)))
+                          (if (string-match "^\\(.*?\\):\\([0-9]+\\)\\(?::\\([0-9]+\\)\\)?$" name)
+                              (cons
+                               (match-string 1 name)
+                               (cons (string-to-number (match-string 2 name))
+                                     (string-to-number (or (match-string 3 name) "")))
+                               )
+                            fn))) files)))
+
+;; TODO add the same function when opening emacs directly
